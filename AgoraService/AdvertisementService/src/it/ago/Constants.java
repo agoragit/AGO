87a6d89f91498c9aa702e9bedaa5fef230c2edc9
dushan <dushan@codegen.net>
2020-01-21 15:18:57 +0530

    AGO:Vehicle constants

--- AgoraService/AdvertisementService/src/it/ago/Constants.java
+++ AgoraService/AdvertisementService/src/it/ago/Constants.java
@@ -3,4 +3,39 @@ package it.ago;
 public class Constants
 {
 	public static String ADV_TYPE_VEHICLE = "vehicle";
+
+
+	public static String VEHI_TRANSMISION_TYPE_MANUAL = "MANUAL";
+	public static String VEHI_TRANSMISION_TYPE_AUTO = "AUTO";
+	public static String VEHI_TRANSMISION_TYPE_TRIPONIC = "TRIPONIC";
+	public static String VEHI_TRANSMISION_TYPE_OTHER = "OTHER";
+
+	public static String VEHI_BODY_TYPE_HATCHBACK = "HATCHBACK";
+	public static String VEHI_BODY_TYPE_SEDAN = "SEDAN";
+	public static String VEHI_BODY_TYPE_MVP = "MVP";
+	public static String VEHI_BODY_TYPE_SUV = "SUV";
+	public static String VEHI_BODY_TYPE_CROSSOVER = "CROSSOVER";
+	public static String VEHI_BODY_TYPE_COUPE = "COUPE";
+	public static String VEHI_BODY_TYPE_CONVERTIABLE = "CONVERTIABLE";
+	public static String VEHI_BODY_TYPE_OTHER= "OTHER";
+
+
+	public static String VEHI_FUEL_TYPE_PETROL = "PERTOL";
+	public static String VEHI_FUEL_TYPE_ELECTRIC = "ELECTRIC";
+	public static String VEHI_FUEL_TYPE_HYBRID= "ELECTRIC";
+	public static String VEHI_FUEL_TYPE_OTHER= "OTHER";
+
+	public static String VEHI_CONDITION_NEW= "NEW";
+	public static String VEHI_CONDITION_USED ="USED";
+	public static String VEHI_CONDITION_RECONDITION ="RECONDITION";
+
+
+
+
+
+
+
+
+
+
 }

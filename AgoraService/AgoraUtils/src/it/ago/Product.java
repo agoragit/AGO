0cb34f5e43a524b1438a21094b82ba20044d7694
dushanwickramasinghe@gmail.com <dushanwickramasinghe@gmail.com>
2020-09-07 20:49:20 +0530

    AGO:AGO:add product categories load

--- AgoraService/AgoraUtils/src/it/ago/Product.java
+++ AgoraService/AgoraUtils/src/it/ago/Product.java
@@ -1,5 +1,6 @@
 package it.ago;
 
+import it.ago.adv.DBQuearies;
 import it.ago.utils.DBConnection;
 import it.ago.utils.db.Savable;
 
@@ -8,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @XmlType(name = "Product", namespace = "http://lass")
 public class Product extends Savable
@@ -15,9 +18,11 @@ public class Product extends Savable
 	private String code;
 	private String name;
 	private int status;
+	private List<ProductCategory> productCategoryList;
 
 	public Product()
 	{
+		productCategoryList = new ArrayList<>();
 	}
 
 	public void checkValidity() throws SQLException
@@ -126,6 +131,10 @@ public class Product extends Savable
 		this.code = rs.getString( "CODE" );
 		this.name = rs.getString( "NAME" );
 
+		if(level > 0 )
+		{
+			loadProductCategories(con);
+		}
 	}
 
 	public String getCode()
@@ -156,6 +165,34 @@ public class Product extends Savable
 	public void setStatus( int status )
 	{
 		this.status = status;
+	}
+
+	private void loadProductCategories(Connection con)
+	{
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try
+		{
+			ps = con.prepareStatement( DBQuearies.Q_LOAD_PRODUCT_CATEGORIES);
+			ps.setString( 1, this.code );
+			rs = ps.executeQuery();
+			while (rs.next())
+			{
+				ProductCategory productCategory = new ProductCategory();
+				productCategory.load(rs);
+				productCategoryList.add(productCategory);
+			}
+
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			DBConnection.close(rs);
+			DBConnection.close(ps);
+		}
 	}
 
 }
--- AgoraService/AgoraUtils/src/it/ago/utils/SystemPropertyCache.java
+++ AgoraService/AgoraUtils/src/it/ago/utils/SystemPropertyCache.java
@@ -1,23 +1,38 @@
 package it.ago.utils;
 
+import it.ago.Product;
+import it.ago.adv.DBQuearies;
+import org.json.JSONException;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class SystemPropertyCache
 {
 	static Properties SYSTEM_PROPERTY = new Properties();
 	static String DB_CONF_PATH = "../conf/db.conf";
+	static List<Product> cashProductsList = new ArrayList<>();
 
 	static
 	{
 		loadDBProperties();
+		loadProductCategories();
 	}
 
 	public static void init()
 	{
-		loadDBProperties();
+		//loadDBProperties();
+	}
+
+	public static List<Product> getCashProductsList() {
+		return cashProductsList;
 	}
 
 	private static void loadDBProperties()
@@ -35,5 +50,31 @@ public class SystemPropertyCache
 			ex.printStackTrace();
 		}
 
+	}
+	public static void loadProductCategories( ) throws JSONException
+	{
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs  = null;
+		try
+		{
+			con = DBConnection.getConnection( DBConnection.MYSQL_CONNECTION_TYPE );
+			ps = con.prepareStatement( DBQuearies.Q_LOAD_ALL_PRODUCTS );
+			rs  = ps.executeQuery();
+			while( rs.next() )
+			{
+				Product product = new Product();
+				product.load( rs, con, 1 );
+				cashProductsList.add(product);
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			DBConnection.close( con,ps,rs );
+		}
 	}
 }
